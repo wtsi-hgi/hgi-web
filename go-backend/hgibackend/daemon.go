08dba6a8ef192e6dce028d3c9230c19fb7f720b5
Joshua C. Randall <jcrandall@alum.mit.edu>
2013-03-11 09:55:14 +0000

    Added swagger config

--- go-backend/hgibackend/daemon.go
+++ go-backend/hgibackend/daemon.go
@@ -9,6 +9,15 @@ import (
 
 func main() {
 	restful.Add(project.NewService())
+
+	config := restful.SwaggerConfig{
+		WebServicesUrl: "http://localhost:8080",
+		ApiPath: "/apidocs.json",
+		SwaggerPath: "/apidocs/",
+		SwaggerFilePath: "/Users/jr17/swagger-ui-1.1.7",
+	}
+	restful.InstallSwaggerService(config)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
