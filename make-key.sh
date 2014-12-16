db834d02b47da48014a5efdee0e5c4cfe46f135d
Christopher Harrison <ch12@sanger.ac.uk>
2014-12-16 11:40:02 +0000

    Private key generation

---
+++ make-key.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Simple private key generator
+
+# AGPLv3 or later
+# Copyright (c) 2014 Genome Research Limited
+
+read -r -d "" USAGE <<EOF
+Usage: make-key.sh [options]
+
+Options:
+  -s SIZE     Key size, in bytes (default 1024)
+  -o KEYFILE  Key file (default hgi-web.key)
+  -h          This useful message
+EOF
+
+# Defaults
+KEYFILE=hgi-web.key
+KEYSIZE=1024
+
+# Get command line options
+NUMBER='^[1-9][0-9]*$'
+while getopts ":s:o:h" opt; do
+  case $opt in
+    s)
+      if ! [[ $OPTARG =~ $NUMBER ]]; then
+        echo "Key size must be a positive integer"
+        exit 1
+      fi
+      KEYSIZE=$OPTARG
+      ;;
+    o)
+      KEYFILE=$OPTARG
+      ;;
+    h)
+      echo "$USAGE"
+      exit 0
+      ;;
+    \?)
+      echo "No such option -$OPTARG"
+      echo "$USAGE"
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument"
+      echo "$USAGE"
+      exit 1
+      ;;
+  esac
+done
+
+# Create key file
+dd if=/dev/random of=$KEYFILE bs=$KEYSIZE count=1 &>/dev/null && \
+ls -l $KEYFILE
