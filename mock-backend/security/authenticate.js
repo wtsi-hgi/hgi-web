04f2f10cd0d10305405c6dc0b671df69ee66aba4
Christopher Harrison <ch12@sanger.ac.uk>
2014-12-23 11:16:29 +0000

    Bearer and basic authentication using token :)

--- mock-backend/security/authenticate.js
+++ mock-backend/security/authenticate.js
@@ -1,10 +1,43 @@
 // AGPLv3 or later
 // Copyright (c) 2014 Genome Research Limited
 
-module.exports = function(req, res, next) {
-  // Vacuous authentication, for now
-  next();
+// Load private key and initialise token codec
+var fs         = require('fs'),
+    privateKey = fs.readFileSync(process.env.KEYFILE),
+    xiongxiong = require('xiongxiong')(privateKey);
 
-  // Not cool
-  // res.status(401).send('Unauthorised');
+module.exports = function(req, res, next) {
+  var passed = false,
+      auth   = (req.headers.authorization || '').split(' '),
+      token;
+
+  switch (auth[0].toLowerCase()) {
+    case 'bearer':
+      // Validate bearer token
+      token  = xiongxiong.extract(auth[1]);
+      passed = token.isValid || false;
+      break;
+
+    case 'basic':
+      // Validate basic auth pair
+      var basicPair = (new Buffer(auth[1], 'base64')).toString().split(':');
+      token  = xiongxiong.extract(basicPair[0], basicPair[1]);
+      passed = token.isValid || false;
+      break;
+
+    default:
+      break;
+  }
+
+  if (passed) {
+    // We're good!
+    // Thread extracted token data through all requests
+    req.token = token;
+    next();
+
+  } else {
+    // Fall back to prompting for basic auth pair
+    res.set('WWW-Authenticate', 'Basic');
+    res.status(401).send('Cannot authenticate');
+  }
 };
